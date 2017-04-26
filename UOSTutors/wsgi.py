dcf7625da54f5d1b6ceb6cb595c8499edfe78afd
Omar Einea <eineao@gmail.com>
2017-04-26 20:33:42 +0400

    Rename Project to UOSTutors

--- UOSTutors/wsgi.py
+++ UOSTutors/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TutorMatcher project.
+WSGI config for UOSTutors project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TutorMatcher.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UOSTutors.settings")
 
 application = get_wsgi_application()
