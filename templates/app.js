f91885f7ab04c9c70169a7f702851f945e044a84
Omar Einea <eineao@gmail.com>
2017-04-27 20:05:06 +0400

    Add Basic Toolbar and Banner

--- templates/app.js
+++ templates/app.js
@@ -1,1 +1,6 @@
-angular.module('UOSTutors', ['ngMaterial']);
+angular.module('UOSTutors', ['ngMaterial'])
+
+.controller('toolbar', function($scope) {
+    $scope.subjects = ['Statistics', 'Accounting', 'Algebra', 'Finance', 'Chemistry',
+                       'Calculus', 'Study Skills', 'Writing', 'Biology', 'Computer Science'];
+});
