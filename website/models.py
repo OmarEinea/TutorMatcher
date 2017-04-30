0ad0d184c4869fe4a31d9bc1802e8800b00730cb
OvuHp <ovuhp@yahoo.com>
2017-04-30 17:49:04 +0400

    Working on db models (Users)

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=25)
+    age = models.CharField(max_length=25)
+    email = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     #url(r'html/[a-zA-Z]+$', views.checker, name='checker'),
+    url(r'^api/get_ctutors/(?P<course>[0-9]+)/?$', views.checker, name='checker'),
     url(r'^html/(?P<filename>[a-zA-Z]+)/?$', views.checker, name='checker'),
     url(r'', views.index, name='index'),
 ]
