8f159dc1c5ab33e898c72cb97a2679c848c37cc2
Omar Einea <eineao@gmail.com>
2017-04-30 15:53:55 +0300

    Start Using AngularJS Templating

--- website/static/app.js
+++ website/static/app.js
@@ -1,6 +1,14 @@
 angular.module('UOSTutors', ['ngMaterial'])
 
+.config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('{$').endSymbol('$}');
+})
+
 .controller('toolbar', function($scope) {
     $scope.subjects = ['Statistics', 'Accounting', 'Algebra', 'Finance', 'Chemistry',
                        'Calculus', 'Study Skills', 'Writing', 'Biology', 'Computer Science'];
+})
+
+.controller('pageLoad', function($scope) {
+    $scope.url = '/html/' + window.location.pathname.split('/')[1];
 });
