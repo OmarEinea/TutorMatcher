a58c5b3de43d76bb1e3c89afb090916f7c4a6c0f
Omar Einea <eineao@gmail.com>
2017-05-09 15:28:52 +0300

    Link Pages & Add Tutors Page

--- website/static/app.js
+++ website/static/app.js
@@ -60,7 +60,7 @@ angular.module('UOSTutors', ['ngMaterial', 'materialCalendar', 'mdCollectionPagi
                        'Calculus', 'Study Skills', 'Writing', 'Biology', 'Computer Science'];
 
     $scope.allCourses = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-    $scope.shownCourses = $scope.allCourses.slice(0, 8);
+    $scope.shownCourses = null;
 
     $scope.catClass = "absolute";
     $window.onscroll = function() {
