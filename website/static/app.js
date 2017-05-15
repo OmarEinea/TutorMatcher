c26c8ee522b6cf11fd38d3daaa0fcd29d1ee2e85
Omar Einea <eineao@gmail.com>
2017-05-15 20:25:10 +0300

    Book a Session & Tutor Calendar Buttons

--- website/static/app.js
+++ website/static/app.js
@@ -6,13 +6,16 @@ angular.module('UOSTutors', ['ngMaterial', 'ngRoute', 'materialCalendar', 'mdCol
     $routeProvider.when("/", {
         templateUrl: '/html/index'
     }).when("/Courses", {
-        templateUrl: '/html/courses'
+        templateUrl: '/html/courses',
+        controller: 'courses'
     }).when("/Tutors", {
         templateUrl: '/html/tutors'
     }).when("/Tutor", {
-        templateUrl: '/html/tutor'
+        templateUrl: '/html/tutor',
+        controller: 'tutor'
     }).when("/Course", {
-        templateUrl: '/html/course'
+        templateUrl: '/html/course',
+        controller: 'course'
     });
 })
 
@@ -51,7 +54,10 @@ angular.module('UOSTutors', ['ngMaterial', 'ngRoute', 'materialCalendar', 'mdCol
     };
 })
 
-.controller("calendar", function($scope, $filter) {
+.controller("tutor", function($scope, $filter) {
+    var randColor = function() {
+        return ["green-500", "red-400"][Math.floor((Math.random()*2))];
+    };
     $scope.dayFormat = "d";
 
     // To select a single date, make sure the ngModel is not an array.
@@ -66,10 +72,16 @@ angular.module('UOSTutors', ['ngMaterial', 'ngRoute', 'materialCalendar', 'mdCol
     $scope.tooltips = true;
     $scope.status = ["Busy Day", "Free Day", "Day Off"];
     $scope.setDayContent = function(date) {
-        var index = Math.floor(Math.random() * 3);
-        return "<h3 layout='column' layout-align='center center' md-colors=\"::{color: '" +
-            (index == 0 ? "red" : (index == 1 ? "green" : "grey")) +
-            "-500'}\">" + $scope.status[index] + "</h3>";
+        return '<div layout="column">' +
+                    '<div layout="row">' +
+                        '<md-button class="md-raised" style="top: 8; left: 8" md-colors="{background: \'' + randColor() + '\'}">2</md-button>' +
+                        '<md-button class="md-raised" style="top: 8; right: 8" md-colors="{background: \'' + randColor() + '\'}">3</md-button>' +
+                    '</div>' +
+                    '<div layout="row">' +
+                        '<md-button class="md-raised" style="bottom: 8; left: 8" md-colors="{background: \'' + randColor() + '\'}">5</md-button>' +
+                        '<md-button class="md-raised" style="bottom: 8; right: 8" md-colors="{background: \'' + randColor() + '\'}">6</md-button>' +
+                    '</div>' +
+                '</div>';
     };
 })
 
@@ -77,7 +89,7 @@ angular.module('UOSTutors', ['ngMaterial', 'ngRoute', 'materialCalendar', 'mdCol
     $scope.subjects = ['Statistics', 'Accounting', 'Algebra', 'Finance', 'Chemistry',
                        'Calculus', 'Study Skills', 'Writing', 'Biology', 'Computer Science'];
 
-    $scope.allCourses = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    $scope.allCourses = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     $scope.shownCourses = null;
 
     $scope.catClass = "absolute";
@@ -87,5 +99,13 @@ angular.module('UOSTutors', ['ngMaterial', 'ngRoute', 'materialCalendar', 'mdCol
             $scope.catClass = catClass;
             $scope.$apply();
         }
+    };
+})
+
+.controller('course', function($scope) {
+    $scope.tutors = new Array(8);
+    $scope.date = new Date();
+    $scope.addToCart = function(session) {
+        
     };
 });
